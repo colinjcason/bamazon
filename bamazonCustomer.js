1000bf3efb3f862c80820b65935e3e799cff626e
Colin Cason <colinjcason@gmail.com>
2019-09-11 15:14:15 -0700

    finished funcionality and updating database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,17 +9,17 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     showProducts();
 });
 
 function showProducts() {
     var query = "SELECT id, name, price FROM products";
-    connection.query(query, function(err, res) {
-        if(err) throw err;
+    connection.query(query, function (err, res) {
+        if (err) throw err;
         console.log("Welcome to Bamazon for customers!\nHere is the list of items available for purchase: ");
-        for(var i = 0; i < res.length; i++) {
+        for (var i = 0; i < res.length; i++) {
             console.log(res[i].id + ".) " + res[i].name + " || Price: " + res[i].price);
         }
 
@@ -27,14 +27,13 @@ function showProducts() {
     });
 }
 
-function startProgram() {           
-    inquirer.prompt([
-        {
+function startProgram() {
+    inquirer.prompt([{
             name: "id",
             message: "Which product would you like to buy?",
             type: "input",
-            validate: function(value) {
-                if(isNaN(value)) {
+            validate: function (value) {
+                if (isNaN(value)) {
                     return console.log("\nPlease enter a valid index");
                 }
                 return true;
@@ -44,20 +43,62 @@ function startProgram() {
             name: "quantity",
             message: "How many would you like to buy?",
             type: "input",
-            validate: function(value) {
-                if(isNaN(value)) {
+            validate: function (value) {
+                if (isNaN(value)) {
                     return console.log("\nPlease enter a number");
                 }
                 return true;
             }
         }
+    ]).then(function (answer) {
+        buyProduct(answer.id, answer.quantity);
+    });
+}
+
+function buyProduct(id, quantity) {
+    connection.query(`SELECT id, name, stock, price FROM products WHERE id = "${id}"`, function (err, res) {
+        if (err) throw err;
+
+        var productName = res[0].name;
+        var price = res[0].price;
+        var stock = res[0].stock;
+
+        if (quantity > stock) {
+            console.log("Sorry! We don't have that many " + productName + "(s) in stock!\nPlease request a smaller amount.");
+            startProgram();
+        } else {
+            connection.query("UPDATE products SET ? WHERE ?",
+                [{
+                        stock: stock - quantity
+                    },
+                    {
+                        name: productName
+                    }
+                ],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("You purchased " + quantity + " " + productName + "(s) " + "for $" + (price * quantity) + "!");
+        shopAgain();
+
+                });
+        };
+    });
+}
+
+function shopAgain() {
+    inquirer.prompt([
+        {
+            name: "shop",
+            message: "Would you like to shop some more?",
+            type: "confirm",
+            default: true
+        }
     ]).then(function(answer) {
-        connection.query(`SELECT id, name, stock, price FROM products WHERE id = "${answer.id}"`, function(err, res) {
-            if(err) throw err;
-
-            console.log(res[0].name);
-        });
-      
-
+        if(answer.shop === true) {
+            showProducts();            
+        }
+        else {
+            connection.end();            
+        }
     });
 }
